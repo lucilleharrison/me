eabd1d92c37b9e16eb5420cd40287fb977e3bd3c
lucilleharrison <lucilleh442@gmail.com>
2023-06-19 15:10:53 +1000

    guess some things

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -16,17 +16,17 @@ import platform
 
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
-
+# i think this line will substitue from this list of words into a sentence when asked
 some_words = ["what", "does", "this", "line", "do", "?"]
-
+# when commanded i think it will print 'word'. 
 for word in some_words:
     print(word)
-
+# i think when encountered by an 'x' in the list, it will print 'x'. 
 for x in some_words:
     print(x)
-
+# i think this commane will print a word from the list 
 print(some_words)
-
+# i think when commanded len, it will print more 'a word from the list, contains more than 3 words' 
 if len(some_words) > 3:
     print("some_words contains more than 3 words")
 
@@ -37,7 +37,8 @@ def usefulFunction():
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
     """
+    # i think this line will print a return string platform.username 
     print(platform.uname())
 
-
+# i think this will return an unuseful function string 
 usefulFunction()
