220af4dc933c3c146b29ed7ea3ed11d03ca696fc
lucilleharrison <lucilleh442@gmail.com>
2023-06-19 15:10:39 +1000

    make a start on this ex

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -66,7 +66,9 @@ def shout(a_string):
     they all do different things. You'll need to actually read the
     docs to find out which one you actually need.
     """
-    the_answer = None
+    #a_string = 'hello'
+    #print(a_string.upper())
+    the_answer = a_string.upper()
     return the_answer
 
 
@@ -79,7 +81,7 @@ def really_shout(a_string):
     that you've reused the function you already wrote.
     Look up how to 'concatinate' strings to make this happen.
     """
-    the_answer = None
+    the_answer = a_string.upper() + '!'
     return the_answer
 
 
@@ -90,12 +92,12 @@ def shout_with_a_number(a_string, a_number):
     'HELLO 42'
 
     HINT: Lookup how to cast a_number to a string or lookup how to use
-          string formatting in python.
-          There are a few ways to do this, so if you're looking for a
-          challenge, see if you can make the test pass with at least two ways
-          of doing the same job.
+    string formatting in python.
+    There are a few ways to do this, so if you're looking for a
+    challenge, see if you can make the test pass with at least two ways
+    of doing the same job.
     """
-    the_answer = None
+    the_answer = a_string.upper + a_number
     return the_answer
 
 
